958c6432ec0213cff70fd362cc1e7a82335dd62b
Brady Griffith <bradyagriffith@gmail.com>
2024-07-05 10:19:57 +0200

    Doctrings and comments for `bgse-computer` cli.

--- src/booms_gse/computer_gse/cli/__init__.py
+++ src/booms_gse/computer_gse/cli/__init__.py
@@ -1,21 +1,23 @@
+"""BOOMS flight computer UPD interaction CLI.
+
+Commands for recording, forwarding, and processing the UDP packets sent or
+logged by the BOOMS flight computer.
+"""
 import asyncio
 from ipaddress import ip_address
 from pathlib import Path
-import typing as t
 
 import click
-from bokeh.server.server import Server
-from bokeh.application import Application
-from bokeh.application.handlers import ScriptHandler
 
 from .. import network
 
-SERIAL_LOG_PATH = Path(__file__).parent/'computer_serial.log'
+SERIAL_LOG_PATH = Path('.')/'computer_serial.log'
 
 
 class IPAddress(click.ParamType):
-    name = 'IP address'
     """Restricts the type to a string for a valid IPv4 or IPv6 address."""
+    name = 'IP address'
+
     def convert(self, value, param, ctx):
         try:
             ip = ip_address(value)
@@ -26,30 +28,31 @@ class IPAddress(click.ParamType):
 
 
 class PacketID(click.IntRange):
+    """Restricts the type to a single byte integer in decimal or hex."""
     name = 'packet ID'
 
     def __init__(self):
         super().__init__(0, 255)
-    
+
     def convert(self, value, param, ctx):
+        """Convert to an integer."""
         if isinstance(value, int):
             return value
-        
+
         try:
             if value[:2].lower() == "0x":
                 return int(value[2:], 16)
-            elif value[:1] == "0":
-                return int(value, 8)
             return int(value, 10)
         except ValueError:
             self.fail(f"{value!r} is not a valid integer", param, ctx)
+
 
 PACKET_ID = PacketID()
 
 
 @click.group(chain=True)
 @click.option('-a', '--address', default='0.0.0.0', type=IPAddress())
-@click.option('-p', '--port', default='20501', type=click.IntRange(0, 65535))
+@click.option('-p', '--port', default='20501', type=click.IntRange(1, 65535))
 @click.option('-f', '--from_file',
               type=click.Path(file_okay=True,
                               dir_okay=False,
@@ -58,13 +61,13 @@ PACKET_ID = PacketID()
               help='Playback speed.', type=click.FloatRange(min_open=0))
 @click.option('--debug', is_flag=True)
 def gse(**kwargs):
-    """Receive UDP datagrams and add to parsers"""
-    # Setup logging
+    """Receive UDP datagrams and pass the packets to processing commands."""
+    # TODO: Setup logging
 
 
 @gse.result_callback()
 def process_pipeline(processors, address, port, from_file, speed, debug):
-    """Sets up the async run."""
+    """Start the run for the receiver and processing asyncio functions."""
     asyncio.run(network.receive_packets(address, port, processors,
                                         from_file, speed))
 
@@ -72,9 +75,8 @@ def process_pipeline(processors, address, port, from_file, speed, debug):
 @gse.command()
 @click.argument('target_address', default='127.0.0.1', type=IPAddress())
 @click.argument('target_port', default='20502', type=click.IntRange(0, 65535))
-# @click.option('--packet_id', help='Forward only this packet type.')
 def forward(target_address, target_port):
-    """Forward the UDP packets"""
+    """Forward the UDP packets to a new IP and port."""
     return network.PacketForwarder(target_address, target_port)
 
 
@@ -82,12 +84,12 @@ def forward(target_address, target_port):
 @click.argument('packet_id', type=PACKET_ID)
 @click.argument('target_serial', type=str)
 def serial(packet_id, target_serial):
-    """Pass the data from a selected device to a serial port"""
+    """Pass the data from a selected device to a serial port."""
     return network.PacketSerial(packet_id, target_serial)
 
 
 @gse.command()
-def psuedoserial(address, port, from_file):
+def pseudoserial(address, port, from_file):
     """Pass networked BOOMS packets to pseudo-serial ports."""
     # log_file_handler = logging.FileHandler(SERIAL_LOG_PATH)
     # log_file_formatter = logging.Formatter(
@@ -99,7 +101,7 @@ def psuedoserial(address, port, from_file):
     #     logging.basicConfig(level=logging.DEBUG)
     #     network.logger.setLevel(logging.DEBUG)
 
-    return network.PacketPsuedoSerial()
+    return network.PacketPseudoSerial()
 
 
 @gse.command()
@@ -108,38 +110,17 @@ def psuedoserial(address, port, from_file):
                                 writable=True,
                                 path_type=Path))
 def record(path):
-    """Write networked BOOMS packets to files for each instrument."""
+    """Write the data for imagers and spectrometers to a file."""
     # network.logger.addHandler(logging.StreamHandler())
     # if kwargs['debug']:
     #     network.logger.setLevel(logging.DEBUG)
     return network.PacketLogger(path)
 
 
-class MMGSEPacket(network.PacketForwarder):
-    def __init__(self):
-        super().__init__('127.0.0.1', 20502)
-        self.server = None
-
-    def setup(self, transport):
-        super().setup(transport)
-
-        mm_gse_path = Path(__file__).parent.parent / 'mm_gse.py'
-        print(mm_gse_path)
-        apps = {'/': Application(ScriptHandler(filename=mm_gse_path))}
-        self.server = Server(apps)
-        self.server.start()
-        url = f"http://localhost:{self.server.port}{self.server.prefix}/"
-        print(f"Bokeh app running at: {url}")
-        # self.server.io_loop.start()
-
-    def stop(self):
-        super().stop()
-        self.server.stop()
-
-
 @gse.command()
 def mm_gse():
-    return MMGSEPacket()
+    """Start an mm_gse bokeh server and forward UDP packets to it."""
+    return network.MMGSEPacket()
 
 
 if __name__ == '__main__':
--- src/booms_gse/computer_gse/network.py
+++ src/booms_gse/computer_gse/network.py
@@ -1,10 +1,20 @@
+"""Tools to process UDP packets from the flight computer.
+
+Generally this can be used by just calling the asynchronous function
+`asyncio.run(receive_packets(ip, port, processors))` with a tuple of
+processors, each instances of `PacketProcessor` or one of its subclasses.
+"""
 import asyncio
+import logging
 import os
 import pty
-import logging
-from pathlib import Path
 from itertools import chain
+from pathlib import Path
+
 import serial
+from bokeh.application import Application
+from bokeh.application.handlers import ScriptHandler
+from bokeh.server.server import Server
 
 BOOMS_SERIAL_DIR = Path(
     os.environ.get('BOOMS_SERIAL_DIR', default='/dev/booms'))
@@ -19,25 +29,42 @@ logger.setLevel(logging.INFO)
 
 
 async def write_with_lock(file, data, lock):
+    """Acquire an async lock before writting and flushing data.
+
+    Args:
+        file: A file like object that has a method write(data).
+        data (bytes): A bytes object to be written.
+        lock (asyncio.Lock): Lock to prevent simultaneous writes.
+    """
     async with lock:
         file.write(data)
         file.flush()
 
 
-class FlightComputerReciever(asyncio.DatagramProtocol):
+class FlightComputerReceiver(asyncio.DatagramProtocol):
+    """Endpoint for flight computer UDP packets that runs a set of processors
+    on each packet received."""
     def __init__(self, processors=None):
+        """Initialize the instance with a set of processors.
+
+        Args:
+            processors (tuple(PacketProcessor)): Each processor should will
+                have received() called for each UDP packet.
+        """
         if processors is None:
-            processors = list()
-        self.processors = processors
+            processors = tuple()
+        self.processors = tuple(processors)
         self.transport = None
 
     def connection_made(self, transport):
+        """Setup the prcessors when a connection is made."""
         self.transport = transport
 
         for p in self.processors:
             p.setup(transport)
 
     def connection_lost(self, exc):
+        """Close the processors when the connection is lost."""
         logger.info('Lost UDP connection')
         if exc is not None:
             logger.warning('UDP closed with exception.', exc_info=exc)
@@ -46,35 +73,62 @@ class FlightComputerReciever(asyncio.DatagramProtocol):
             p.close()
 
     def datagram_received(self, data, addr):
+        """Run the processors for each packet received."""
         logger.debug('Datagram received from %s', addr)
 
         for p in self.processors:
             p.receive(data)
 
     def error_received(self, exc):
+        """Log any errors that occur."""
         logger.warning('Flight Computer UDP error received', exc_info=exc)
 
 
 class PacketProcessor:
+    """Contain an operation to be run on packets as they're received."""
     def __init__(self):
         self.transport = None
 
     def setup(self, transport):
+        """Initialize anything needed for packet processing.
+
+        Args:
+            transport (DatagramTransport): The transport packets are coming in
+                on. It is saved for optional use in processing.
+        """
         self.transport = transport
 
     def receive(self, packet):
-        pass
+        """The function run on each packet.
+
+        Args:
+        packet (bytes): The entire UDP packet received. Should be overloaded by
+            subclasses.
+        """
 
     def close(self):
+        """Clear the transport variable. Should be overloaded with cleanup
+        when needed by a subclass.
+        """
         # Leave closing transport to the DatagramProtocol instance
         self.transport = None
 
 
 class PacketSerial(PacketProcessor):
+    """Forward the contents of the frame for a selected imager or spectrometer
+    to a serial port."""
     SERIAL_CONFIGS = {'imag': {'baudrate': 230400},
                       'spec': {'baudrate': 38400}}
 
     def __init__(self, device_id, serial_port):
+        """Initialize the serial packet processor
+
+        Args:
+            device_id (int): The device ID set in the flight software. It
+                should be one byte (0 <= device_id < 256). Imagers will start
+                with the first four bits following the pattern 0xC* and
+                spectrometers 0xD*.
+        """
         super().__init__()
 
         self.serial_lock = asyncio.Lock()
@@ -83,73 +137,107 @@ class PacketSerial(PacketProcessor):
         self.serial = None
 
     def setup(self, transport):
+        """Setup the serial port target for packets."""
+        # Serial config should match the instrument type. Follow the pattern
+        #     set by the first four bits.
         if (self.device_id & 0xF0) == 0xC0:
             serial_kwargs = self.SERIAL_CONFIGS['imag']
         elif (self.device_id & 0xF0) == 0xD0:
             serial_kwargs = self.SERIAL_CONFIGS['spec']
         else:
+            # Other devices are not supported yet.
             raise RuntimeError(f'Byte id {self.device_id} is not a supported '
                                'device. Must be imager (0xC*) or spectrometer '
                                '(0xD*).')
 
-        self.serial = serial.Serial(port=self.serial_port,
-                                    **serial_kwargs)
+        self.serial = serial.Serial(port=self.serial_port, **serial_kwargs)
 
         if self.serial is None:
+            # Triggered if there was an error setting up the serial connection.
             raise RuntimeError('Serial not initialized.')
 
     def receive(self, packet):
+        """Write the packet to the serial port."""
         if len(packet) < 16:
-            logger.warning('Packet is less than 16 bytes. Skipped.')
+            logger.warning('Packet is too short for header (<16 bytes).')
 
         if packet[4] == self.device_id:
             if self.serial is None:
+                # Triggered if self.setup() was skipped or self.close()
+                #    has been called.
                 raise RuntimeError('Serial not initialized.')
-    
+
             asyncio.create_task(
                 write_with_lock(self.serial, packet[16:], self.serial_lock)
             )
 
     def close(self):
+        """Close the serial connection."""
         if self.serial is not None:
             self.serial.close()
         super().close()
 
 
 class PacketForwarder(PacketProcessor):
+    """Forward all UPD packets to a new IP and port."""
     def __init__(self, target_ip, target_port):
-        super()
+        """Initialize to forward UDP packets to the set IP and port.
+
+        Args:
+            target_ip (ip_address): Valid IPv4 or IPv6 address to receive
+                the packets.
+            target_port (int): Valid UDP port, 0 < target_port <= 65535)
+        """
+        super().__init__()
 
         self.target_ip = target_ip
         self.target_port = target_port
 
     def receive(self, packet):
+        """Forward the packet to the configured IP and port"""
         self.transport.sendto(packet, (self.target_ip, self.target_port))
 
 
 class PacketLogger(PacketProcessor):
+    """Write the packet contents for each imager and spectrometer to files."""
     IMAG_IDS = set([0xC0 + i for i in range(7)])
     SPEC_IDS = set([0xD0 + i for i in range(3)])
 
     def __init__(self, path_root=None, path_dict=None, fd_dict=None):
-        super()
-    
+        """Initialize the file writter.
+
+        Args:
+            path_root (Path): Parent directory to contain imager and
+                spectrometer log files. If it doesn't exist, it will be
+                created.
+            path_dict (dict): Dictionary with keys of instrument IDs as int
+                matched to a path where that instrument's data will be
+                recorded. If the file doesn't exist, it will be created.
+            fd_dict (dict): Dictionary with keys of instrument IDs as int
+                matched to a file descriptor where that instrument's data
+                will be recorded.
+        """
+        super().__init__()
+
         if path_root is not None:
+            # Set the paths for each instrument in the path_root directory
             Path(path_root).mkdir(exist_ok=True)
             if path_dict is not None:
                 raise ValueError('Only one output file option may be selected')
 
             path_dict = dict()
+            # Imagers:
             for spec_id in self.SPEC_IDS:
                 path_dict[spec_id] = \
                     Path(path_root) / f'spec_{spec_id & 0x0F:}.dat'
-
+            # Spectrometers:
             for imag_id in self.IMAG_IDS:
                 path_dict[imag_id] = \
                     Path(path_root) / f'imag_{imag_id & 0x0F:}.dat'
         logger.debug('Paths: %s', path_dict)
 
         if path_dict is not None:
+            # Get file descriptors for each instrument log path.
             if fd_dict is not None:
                 raise ValueError('Only one output file option may be selected')
             fd_dict = dict()
@@ -159,14 +247,18 @@ class PacketLogger(PacketProcessor):
                 fd_dict[device_id] = \
                     os.open(path, os.O_CREAT | os.O_WRONLY | os.O_NONBLOCK)
 
+        # Make sure only imagers and spectrometer ids are provided. Not all
+        # are needed, but no other id may be added.
         if set(fd_dict.keys()) > self.SPEC_IDS.union(self.IMAG_IDS):
             raise ValueError('Invalid device id provided.')
         self.fds = fd_dict.copy()
         self.f_lock = asyncio.Lock()
         logger.debug('File Descriptors: %s', self.fds)
+        # Dictionary to hold file objects once opened with self.setup()
         self.open_files = {k: None for k in self.fds.keys()}
 
     def setup(self, transport):
+        """Open all of the instrument log files."""
         super().setup(transport)
 
         self.open_files.update(
@@ -174,14 +266,17 @@ class PacketLogger(PacketProcessor):
              for device_id, fd in self.fds.items()})
 
     def close(self):
-        # Cleanup Open Files
+        """Close all of the open instrument log files."""
         for device_id, file in self.open_files.items():
             if file is not None:
                 file.close()
             self.open_files[device_id] = None
 
     def receive(self, packet):
-        # Write to files
+        """Write the packet to it's appropriate instrument log file."""
+        if len(packet) < 16:
+            logger.warning('Packet is too short for header (<16 bytes).')
+
         id_byte = packet[4]
         logger.debug('ID Byte: %s', hex(id_byte))
 
@@ -193,8 +288,11 @@ class PacketLogger(PacketProcessor):
             # logger.warning('File for %s is closed', hex(id_byte))
 
 
-class PacketPsuedoSerial(PacketLogger):
+class PacketPseudoSerial(PacketLogger):
+    """Process instruments with log file recorder, but the log files are
+        connected to a pseudo serial port."""
     def __init__(self):
+        """Initialize the processor and pick the pseudo serial ports."""
         self.serial = dict()
 
         fd_dict = dict()
@@ -206,17 +304,46 @@ class PacketPsuedoSerial(PacketLogger):
         super().__init__(fd_dict=fd_dict)
 
 
+class MMGSEPacket(PacketForwarder):
+    """Process the packets by forwarding them to a loopback address, and start
+    an mm_gse bokeh server listening to that port."""
+    def __init__(self):
+        """Initialize the processor and set up a forwarding processor to
+            the mm_gse server."""
+        super().__init__('127.0.0.1', 20502)
+        self.server = None
+
+    def setup(self, transport):
+        """Start the packer forwarder and start the bokeh server for mm_gse."""
+        super().setup(transport)
+
+        mm_gse_path = Path(__file__).parent.parent / 'mm_gse.py'
+        print(mm_gse_path)
+        apps = {'/': Application(ScriptHandler(filename=mm_gse_path))}
+        self.server = Server(apps)
+        self.server.start()
+        url = f"http://localhost:{self.server.port}{self.server.prefix}/"
+        print(f"Bokeh app running at: {url}")
+
+    def close(self):
+        """Close the packet forwarder and the bokeh server."""
+        super().close()
+        self.server.stop()
+
+
 async def receive_packets(ip_addr, port, processors=None,
                           from_file=None, speed=1.0):
+    """Start an async loop to receive all incoming UDP packers and run
+        the processors provided on each of them."""
     loop = asyncio.get_running_loop()
     on_con_lost = loop.create_future()
 
     def create_endpoint(processors=processors):
-        return FlightComputerReciever(processors)
+        return FlightComputerReceiver(processors)
 
-    transport, comp_ser = \
-        await loop.create_datagram_endpoint(create_endpoint,
-                                            local_addr=(ip_addr, port))
+    transport, _ = await loop.create_datagram_endpoint(create_endpoint,
+                                                       local_addr=(ip_addr,
+                                                                   port))
 
     try:
         if from_file is not None:
@@ -225,12 +352,14 @@ async def receive_packets(ip_addr, port, processors=None,
         else:
             await on_con_lost
     except asyncio.exceptions.CancelledError:
-        logger.debug('Cancelled', stack_info=True, exc_info=True)            
+        logger.debug('Cancelled', stack_info=True, exc_info=True)
     finally:
         transport.close()
 
 
 async def start_playback(file, **kwargs):
+    """Start an asyncio thread calling the middleman playback binary to
+        replay a flight computer data file."""
     if not PLAYBACK_PATH.exists():
         raise RuntimeError('Could not find playback executable. '
                            'Set env variable BOOMS_PLAYBACK.')
